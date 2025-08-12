7cb48f52afc2fffa83ee30e4ca4c2b749e61d8fd
isadoratoss <isadoratoss@gmail.com>
2025-08-11 22:00:12 -0300

    Commit 11/08, implementacao category

---
+++ src/cases/categories/category.service.ts
@@ -0,0 +1,26 @@
+import { Repository } from "typeorm";
+import { Category } from "./category.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Injectable } from "@nestjs/common";
+
+@Injectable ()
+export class CategoryService {
+
+    constructor(
+        private repository: Repository<Category>
+    ){}
+    findAll():Promise<Category[]>{
+        return this.repository.find();
+    }
+
+    findById(id: string) :Promise<Category>{
+        return this.repository.findOneBy({id : id});
+
+    }
+    save(category: Category) :Promise<Category>{
+        return this.repository.save(category)
+    }
+   async remove(id: string): Promise<void> {
+    await this.repository.delete(id);
+    }
+}
