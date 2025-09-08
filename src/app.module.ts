6a135d46978a9dfe8b5b0508e96d73268ff6088a
isadoratoss <isadoratoss@gmail.com>
2025-09-07 21:09:58 -0300

    Aula 01/09/2025

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryModule } from './cases/categories/category.module';
 import { BrandModule } from './cases/brands/brand.module';
-import { CustomerModule } from './cases/customers/customer.module';
+import { CustomerModule } from './customers/customer.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
--- src/customers/customer.module.ts
+++ src/customers/customer.module.ts
@@ -1,12 +1,35 @@
 import { Module } from "@nestjs/common";
+
+
+
 import { TypeOrmModule } from "@nestjs/typeorm";
+
+
 import { Customer } from "./customer.entity";
+
+
 import { CustomerService } from "./customer.service";
+
+
 import { CustomerController } from "./customer.controller";
 
+
+
+
+
 @Module({
+
+
   imports: [TypeOrmModule.forFeature([Customer])],
+
+
   providers: [CustomerService],
+
+
   controllers: [CustomerController]
+
+
 })
+
+
 export class CustomerModule {}
